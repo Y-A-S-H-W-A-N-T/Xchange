704a68910df36332741a64e1a879db9a1d7c6b4b
raoyashwant <raoyashwant132@gmail.com>
2024-01-29 01:08:45 +0530

    buy and sell card added

--- Front/screens/home.js
+++ Front/screens/home.js
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native'
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import SELL from '../assets/sell.png'
+import BUY from '../assets/buy1.png'
 import React from 'react'
 
 export default function Home(){
   return (
     <View>
-      <Text>Welcome to Home Page.</Text>
+      <View style={styles.card}>
+        <TouchableOpacity>
+          <Image
+            style={styles.sell}
+            source={SELL}
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>Sell a product</Text>
+      </View>
+      
+      <View style={styles.card}>
+        <Text style={styles.text}>Buy a product</Text>
+        <TouchableOpacity>
+          <Image
+            style={styles.buy}
+            source={BUY}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  sell: {
+    height: 150,
+    width: 200,
+    borderRadius: 30,
+    borderWidth: 7,
+    borderColor: 'black'
+  },
+  buy: {
+    height: 150,
+    width: 200,
+    borderRadius: 30,
+    borderWidth: 7,
+    borderColor: 'black'
+  },
+  card:{
+    borderRadius: 30,
+    marginTop: 130,
+    marginLeft: 20,
+    flexDirection: 'row',
+  },
+  text:{
+    color: 'green',
+    marginTop: '18%',
+    margin: 30
+  }
+})
